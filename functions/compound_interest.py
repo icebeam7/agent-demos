cd46f5724616191aa9cc67f75ffd344b4081e9c3
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2025-06-09 01:32:45 +0200

    Return JSON object in user function

--- functions/compound_interest.py
+++ functions/compound_interest.py
@@ -1,3 +1,5 @@
+import json
+
 class CompoundInterest:
     def calculate_future_value(current_price: float, 
                                monthly_interest_rate: float = 0.02, 
@@ -12,4 +14,4 @@ class CompoundInterest:
         """
         future_value = current_price * ((1 + monthly_interest_rate) ** months)
         print(f"Future value calculated: {future_value} for current price: {current_price}")
-        return str(future_value)
+        return json.dumps({"future_value": future_value})
